8da351098d6830b99bb9d11b2f55a453b7ec120c
BigNATE38 <natevandy38@gmail.com>
2023-06-05 22:05:05 -0700

    USer

---
+++ Main/server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+// const { gql } = require("apollo-server-express"); 
+
+// const typeDefs = gql`
+//     type User {
+//         _id: ID 
+//         username: String
+//         email: String
+//     }
+    
+//     type Query {
+//         me: User 
+//         user(username: String!): User
+//     }
+
+//     type Mutation {
+//         loginUser(email: String!, password: String!): Auth
+//         addUser(username: String!, email: String!, password: String!): Auth
+//     }
+
+//     type Auth {
+//         token: ID!
+//         user: User
+//     }
+//     `;
+
+//     module.exports = typeDefs; 
