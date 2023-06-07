220a96ab5ac353d431c594ac36992b4330cee1cc
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-06 21:00:10 -0500

    semi finished about page for now, needs data

--- Main/client/src/pages/About.js
+++ Main/client/src/pages/About.js
@@ -2,22 +2,18 @@ import React from 'react';
 
 export default function About() {
 	return (
-		<div classNameName="aboutUs">
-			<h1 className='team'>Meet the Team</h1>
-			<div className="row cards">
-				<div className="column">
-					<div className="card">
-						<img src="./images/jack.jpg" alt="Jack" width="100%" />
-						<div className="container">
-							<h2>Jack Nguyen</h2>
-							<p className="title">Team Member</p>
-							<p>Some text that describes me lorem ipsum ipsum lorem.</p>
-							<p>example@example.com</p>
-							<p><button className="button">Contact</button></p>
-						</div>
-					</div>
-				</div>
+		<div className="aboutUs">
 
+			<div>
+				<h2 className='team'>About Fleet</h2>
+				<p className="aboutPage mb-3 text-center decoration-4 text-2xl font-bold">At Fleet, we are dedicated to providing our customers with the
+					best experience possible when it comes to trip planning.
+				</p>
+			</div>
+
+			<h1 className='team'>Meet the Team</h1>
+
+			<div className="row cards">
 				<div className="column">
 					<div className="card">
 						<img src="./images/maggie.jpg" alt="Maggie" width="100%" />
@@ -54,6 +50,19 @@ export default function About() {
 							<p>example@example.com</p>
 							<p><button className="button">Contact</button></p>
 						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="column">
+				<div className="card">
+					<img src="./images/jack.jpg" alt="Jack" width="100%" />
+					<div className="container">
+						<h2>Jack Nguyen</h2>
+						<p className="title">Team Member</p>
+						<p>Some text that describes me lorem ipsum ipsum lorem.</p>
+						<p>example@example.com</p>
+						<p><button className="button">Contact</button></p>
 					</div>
 				</div>
 			</div>
