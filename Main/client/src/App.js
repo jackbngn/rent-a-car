74dc80dbee2117654a7e78c40bce70a5ba107ef4
Jack <jackngn83@gmail.com>
2023-06-07 18:58:34 -0700

    HomePage Features

--- Main/client/src/App.js
+++ Main/client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/Navbar';
-import Home from './pages/Home/Home';
+import Home from './pages/Home';
 import About from './pages/About';
 import Reservation from './pages/Reservations';
 import Collection from './pages/Collection';
@@ -10,34 +10,34 @@ import Contact from './pages/Contact';
 import Checkout from './pages/Checkout';
 import './App.css';
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import Footer from './components/FooterSection/Footer';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
 const httpLink = createHttpLink({
-  uri: '/graphql',
+	uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem('id_token');
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
 });
 
 /* export default function App() {
@@ -84,6 +84,7 @@ function App() {
 					<Route path="/contact" element={<Contact />} />
 					<Route path="/checkout" element={<Checkout />} />
 				</Routes>
+				<Footer />
 			</div>
 		</Router>
 	);
--- Main/client/src/pages/Home.js
+++ Main/client/src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import HeroSection from '../components/HeroSection/HeroSection';
+import BookingForm from '../components/BookingForm/BookingForm';
+import Testimonial from '../components/Testimonial/Testimonials';
+import WhyChooseUs from '../components/WhyChooseUs/WhyChooseUs';
 
 export default function Home() {
 	return (
-		<div className='text-center'>
-			<h1>Hello World!</h1>
+		<div className="homePage">
+			<HeroSection />
+			<BookingForm />
+			<WhyChooseUs />
+			<Testimonial />
 		</div>
 	);
 }
