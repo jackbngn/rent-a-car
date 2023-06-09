320b5a0d82fc1b742fb8ebc8e6a7bb787800c6dc
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-08 21:03:30 -0500

    updated cards and adjust footer for about page

--- Main/client/src/pages/About.js
+++ Main/client/src/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+let s = "console.log(Hello World)";
+
 export default function About() {
 	return (
-		<div className="aboutUs">
+		<div id='content-wrap' className="aboutUs">
 			<div>
 				<h2 className='team'>About Fleet</h2>
 				<p className="aboutPage mb-3 text-center decoration-4 text-2xl font-bold">At Fleet, we are dedicated to providing our customers with the
@@ -32,7 +34,7 @@ export default function About() {
 						<img src="./images/nathaniel.jpg" alt="Nathaniel" width="100%" />
 						<div className="container">
 							<h2 className='font-bold text-lg'>Nathaniel Vanderpoort</h2>
-							<p className="title">Team Member</p>
+							<p className="title">Lakewood, CA</p>
 							<p>“2023’s most handsome coder”
 								-my grandma</p>
 							<a className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" href="mailto: natevandy38@gmail.com">Send Email</a>
@@ -45,7 +47,7 @@ export default function About() {
 						<img src="./images/antonio.jpg" alt="Antonio" width="100%" />
 						<div className="container">
 							<h2 className='font-bold text-lg'>Antonio Robledo-Ysasaga</h2>
-							<p className="title">Team Member</p>
+							<p className="title">Irvine, CA</p>
 							<p>How the heck do you center this div?</p>
 							<a className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" href="mailto: robledoy@uci.edu">Send Email</a>
 						</div>
@@ -58,9 +60,9 @@ export default function About() {
 					<img src="./images/jack.jpg" alt="Jack" width="100%" />
 					<div className="container">
 						<h2 className='font-bold text-lg'>Jack Nguyen</h2>
-						<p className="title">Team Member</p>
-						<p>Some text that describes me lorem ipsum ipsum lorem.</p>
-						<a className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" href="mailto: maegan.batorek@valpo.edu">Send Email</a>
+						<p className="title">Fountain Valley, CA</p>
+						<p>{s}</p>
+						<a className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" href="mailto: jackngn83@gmail.com">Send Email</a>
 					</div>
 				</div>
 			</div>
