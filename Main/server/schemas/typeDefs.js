712844102302ad8d7b4ca478ed3ca2654f3a091c
BigNATE38 <natevandy38@gmail.com>
2023-06-08 18:07:32 -0700

    Vehicle.js Syntax Changed and typeDef

--- Main/server/schemas/typeDefs.js
+++ Main/server/schemas/typeDefs.js
@@ -34,9 +34,8 @@ const typeDefs = gql`
 
     type Mutation {
         login(email: String!, password: String!): Auth
-        addReservation(make: String!, model: String! year: Int!, license: String!, color: String!, 
-            numberofSeats: Int!, transmission: String!, engine: String! vehicleClass: String! image: String!)
-        deleteReservation(license: ID!): Vehicle
+        addVehicle(make: String!, model: String! year: Int!, license: String!, color: String!, numberofSeats: Int!, transmission: String!, engine: String! vehicleClass: String! image: String!): User
+        removeVehicle(license: ID!): Vehicle
     }
 `;
 
