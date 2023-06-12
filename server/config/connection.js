5a9d699101d8fa223051e8c62e93c87dd5e096aa
Jack <jackngn83@gmail.com>
2023-06-12 01:13:44 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost//rent-a-car');
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rent-a-car',
+);
 
 module.exports = mongoose.connection;
