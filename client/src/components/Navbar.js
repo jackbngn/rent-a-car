54c9bff7e033404ede3cacd300fd99668b447a11
Jack <jackngn83@gmail.com>
2023-07-28 18:42:02 -0700

    fixed stripe api key

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const Navbar = () => {
-    return (
-        <main>
-            <div className = 'flex-row justify-center'>
-                <div className='col-12 col-md-10 my-3'>
-                    Navbar Section
-                </div>
-            </div>
-        </main>
-    )
-}
+	return (
+		<main>
+			<div className="flex-row justify-center">
+				<div className="col-12 col-md-10 my-3">Navbar Section</div>
+			</div>
+		</main>
+	);
+};
 
 export default Navbar;
